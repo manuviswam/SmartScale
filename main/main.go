c93d7737394b244073b528ba7ea99f12520cc050
Manu Viswam <evmanu@gmail.com>
2016-08-03 11:45:52 +0530

    Manu | Introduce gorilla mux and handlers

--- main/main.go
+++ main/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"net/http"
+	"log"
+	"os"
 	h "github.com/manuviswam/SmartScale/handlers"
+
+	"github.com/gorilla/mux"
+	gh "github.com/gorilla/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", h.ServeIndexPage)
-    http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+    r.HandleFunc("/", h.ServeIndexPage)
+    log.Fatal(http.ListenAndServe(":8080", gh.LoggingHandler(os.Stdout, r)))
 }
