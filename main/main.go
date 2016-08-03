ac00e10836b4191dea10741b926e347256c6065e
Vinay Kaimal <vinaykka@thoughtworks.com>
2016-08-03 14:41:49 +0530

    add logging

--- main/main.go
+++ main/main.go
@@ -12,7 +12,15 @@ import (
 )
 
 func main() {
+	f, err := os.OpenFile("SmartScale.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+	    log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
 	r := mux.NewRouter()
     r.HandleFunc("/", h.ServeIndexPage())
-    log.Fatal(http.ListenAndServe(":8080", gh.LoggingHandler(os.Stdout, r)))
+    log.Fatal(http.ListenAndServe(":8080", gh.LoggingHandler(f, r)))
 }
