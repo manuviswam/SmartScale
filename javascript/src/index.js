5cbf11b28ae77674b7aab680f7549d641e6261f0
Manu Viswam <evmanu@gmail.com>
2016-08-03 10:08:49 +0530

    Manu | Some customizations to chart

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -4,22 +4,54 @@ import ReactDOM from 'react-dom';
 import { LineChart, Line, CartesianGrid, YAxis, XAxis, Tooltip } from "recharts";
 
 const data = [
-	{date:"Mon", weight:50 },
-	{date:"Tue", weight:60 },
-	{date:"Wed", weight:54 },
-	{date:"Fri", weight:55 },
-	{date:"Sat", weight:58 },
-	{date:"Sun", weight:53 }
+	{date:"10-07-2016", weight:50 },
+	{date:"11-07-2016", weight:60 },
+	{date:"12-07-2016", weight:54 },
+	{date:"15-07-2016", weight:55 },
+	{date:"18-07-2016", weight:58 },
+	{date:"20-07-2016", weight:53 }
 ]
+
+const CustomizedLabel = React.createClass({
+  render () {
+    const {x, y, stroke, payload} = this.props;
+		
+   	return <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle">{payload.value}</text>
+  }
+});
+
+const CustomizedXAxisTick = React.createClass({
+  render () {
+    const {x, y, stroke, payload} = this.props;
+		
+   	return (
+    	<g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-35)" fontSize="10">{payload.value}</text>
+      </g>
+    );
+  }
+});
+
+const CustomizedYAxisTick = React.createClass({
+  render () {
+    const {x, y, stroke, payload} = this.props;
+		
+   	return (
+    	<g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={16} textAnchor="end" fill="#666" fontSize="10">{payload.value}</text>
+      </g>
+    );
+  }
+});
 
 const SimpleLineChart = React.createClass({
 	render() {
 		return (
-			<LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-			  <Line type="monotone" dataKey="weight" stroke="#8884d8" />
+			<LineChart width={600} height={300} data={data} margin={{ top: 20, right: 40, bottom: 5, left: 0 }} >
+			  <Line type="monotone" dataKey="weight" stroke="#8884d8" xAxisId="dateAxis" yAxisId="weightAxis" unit="Kg" label={<CustomizedLabel />}/>
 			  <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-			  <XAxis dataKey="date" />
-			  <YAxis />
+			  <XAxis dataKey="date" xAxisId="dateAxis" height={60} tick={<CustomizedXAxisTick/>} />
+			  <YAxis yAxisId="weightAxis" domain={['dataMin - 10', 'dataMax + 10']} tick={<CustomizedYAxisTick/>} />
 			  <Tooltip />
 			</LineChart>
 		);
