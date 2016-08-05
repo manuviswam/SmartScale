c10ce827bc813ebffda89f59e247c55ffd83b344
Manu Viswam <evmanu@gmail.com>
2016-08-05 16:33:23 +0530

    Manu | Add RFID reader logic to arduino

--- hardware/arduino/arduino.ino
+++ hardware/arduino/arduino.ino
@@ -1,15 +1,54 @@
 #include "HX711.h"
 #include "SoftwareSerial.h"
 
+#define MAX_BITS 100                 // max number of bits 
+#define WEIGAND_WAIT_TIME  3000      // time to wait for another weigand pulse.  
+
+SoftwareSerial nodeSerial(5, 4); 
+
+String url="http://10.132.127.136:8080/api/weight";
+long weight = 0;
+
+unsigned char databits[MAX_BITS];    // stores all of the data bits
+unsigned char bitCount;              // number of bits currently captured
+unsigned char flagDone;              // goes low when data is currently being captured
+unsigned int weigand_counter;        // countdown until we assume there are no more bits 
+unsigned long facilityCode=0;        // decoded facility code
+unsigned long cardCode=0;            // decoded card code
+ 
 // HX711.DOUT - pin #A1
 // HX711.PD_SCK - pin #A0
 
 HX711 scale(A1, A0);    // parameter "gain" is ommited; the default value 128 is used by the library
-SoftwareSerial nodeSerial(5, 4);
 
-void setup() {
+void ISR_INT0(){
+  bitCount++;
+  flagDone = 0;
+  weigand_counter = WEIGAND_WAIT_TIME;  
+}
+ 
+
+void ISR_INT1(){
+  databits[bitCount] = 1;
+  bitCount++;
+  flagDone = 0;
+  weigand_counter = WEIGAND_WAIT_TIME;  
+}
+ 
+void setup(){
+  pinMode(2, INPUT);     // DATA0 (INT0)
+  pinMode(3, INPUT);     // DATA1 (INT1)
+
+  nodeSerial.begin(9600);
+  nodeSerial.println("$*"); 
+  
+  digitalWrite(4, HIGH);
+  attachInterrupt(0, ISR_INT0, FALLING);  
+  attachInterrupt(1, ISR_INT1, FALLING);
+ 
+  weigand_counter = WEIGAND_WAIT_TIME;
+
   Serial.begin(9600);
-  Serial.println("HX711 Demo");
 
   Serial.println("Before setting up the scale:");
   Serial.print("read: \t\t");
@@ -44,17 +83,63 @@ void setup() {
             // by the SCALE parameter set with set_scale
 
   Serial.println("Readings:");
-  nodeSerial.begin(9600);
 }
-
-void loop() {
-  Serial.print("one reading:\t");
-  Serial.print(scale.get_units(), 1);
-  Serial.print("\t| average:\t");
-  Serial.println(scale.get_units(10), 1);
-  nodeSerial.println(scale.get_units());
-
-  scale.power_down();             // put the ADC in sleep mode
-  delay(5000);
-  scale.power_up();
+ 
+void loop(){
+  // This waits to make sure that there have been no more data pulses before processing data
+  if (!flagDone) {
+    if (--weigand_counter == 0)
+      flagDone = 1;  
+  }
+ 
+  // if we have bits and we the weigand counter went out
+  if (bitCount > 0 && flagDone) {
+    unsigned char i; 
+    if (bitCount == 35){
+      for (i=2; i<14; i++){
+         facilityCode <<=1;
+         facilityCode |= databits[i];
+      }
+      for (i=14; i<34; i++){
+         cardCode <<=1;
+         cardCode |= databits[i];
+      }
+      printBits();
+    }
+    else if (bitCount == 26){
+      for (i=1; i<9; i++){
+         facilityCode <<=1;
+         facilityCode |= databits[i];
+      }
+      for (i=9; i<25; i++){
+         cardCode <<=1;
+         cardCode |= databits[i];
+      }
+      printBits();  
+    }
+    else {
+     nodeSerial.println("$*"); 
+    }
+    // cleanup and get ready for the next card
+    bitCount = 0;
+    facilityCode = 0;
+    cardCode = 0;
+    for (i=0; i<MAX_BITS; i++){
+       databits[i] = 0;
+    }
+  }
+}
+ 
+void printBits(){    
+      scale.power_up();
+      weight = scale.get_units(10)/10;
+      Serial.println(weight, 1);
+      scale.power_down();             // put the ADC in sleep mode
+      url.concat("?internalNumber=");
+      url.concat(cardCode);
+      url.concat("&weight=");
+      url.concat(weight);
+      nodeSerial.println(url);
+      url="http://10.132.127.136:8080/api/weight";
+      weight = 0;  
 }
